eed0c78412ce14792e994ea67de29910fb532111
Caden Kriese <13630061+cadenkriese@users.noreply.github.com>
2022-01-08 03:46:02 -0700

    Refactor, implement anvil naming

--- src/main/java/org/peaksmp/tweaks/Tweaks.java
+++ src/main/java/org/peaksmp/tweaks/Tweaks.java
@@ -1,11 +1,14 @@
-package org.peaksmp.touchups;
+package org.peaksmp.tweaks;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.kyori.adventure.platform.fabric.FabricServerAudiences;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.screen.AnvilScreenHandler;
+import net.minecraft.world.chunk.ChunkManager;
 
-public class Touchups implements DedicatedServerModInitializer {
-    private static Touchups touchups;
+public class Tweaks implements DedicatedServerModInitializer {
+    private static Tweaks tweaks;
     private FabricServerAudiences adventure;
 
     public FabricServerAudiences adventure() {
@@ -13,13 +16,13 @@ public class Touchups implements DedicatedServerModInitializer {
         return adventure;
     }
 
-    public static Touchups get() {
-        return touchups;
+    public static Tweaks get() {
+        return tweaks;
     }
 
     @Override
     public void onInitializeServer() {
-        touchups = this;
+        tweaks = this;
 
         ServerLifecycleEvents.SERVER_STARTING.register(server -> this.adventure = FabricServerAudiences.of(server));
         ServerLifecycleEvents.SERVER_STOPPED.register(server -> this.adventure = null);
---
+++ src/main/java/org/peaksmp/tweaks/mixin/AnvilScreenMixin.java
@@ -0,0 +1,19 @@
+package org.peaksmp.tweaks.mixin;
+
+import net.minecraft.client.gui.screen.ingame.AnvilScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AnvilScreen.class)
+public class AnvilScreenMixin {
+    @Shadow private TextFieldWidget nameField;
+
+    @Inject(at = @At("RETURN"), method = "setup")
+    public void setup(CallbackInfo info){
+        nameField.setMaxLength(255);
+    }
+}
